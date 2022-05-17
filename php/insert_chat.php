979907275e8179daa2c7c41fd58237f8e5ca6afa
Anshul <62570770+ANSHUL22122001@users.noreply.github.com>
2022-05-17 14:50:51 +0530

    Update insert_chat.php

--- php/insert_chat.php
+++ php/insert_chat.php
@@ -11,7 +11,14 @@ session_start();
     $msg = $_POST['msg'];
 
     if(!empty($msg)){
-        $query1 = "insert into messages(SenderId, SenderName, RecieverId, RecieverName, Msg) VALUES ('$sender_id', '$sender_name', '$reciever_id', '$reciever_name', '$msg')";
+        $ciphering = "AES-128-CTR";
+        $options = 0;
+        $encryption_iv = '1234567891011121';
+        $encryption_key = "Password";
+        $encryption = openssl_encrypt($msg, $ciphering, $encryption_key, $options, $encryption_iv);
+        
+        $query1 = "insert into messages(SenderId, SenderName, RecieverId, RecieverName, Msg) VALUES ('$sender_id', '$sender_name', '$reciever_id', '$reciever_name', '$encryption')";
+        
         $check1 = mysqli_query($con, $query1);
         if($check1){
             echo "success";
